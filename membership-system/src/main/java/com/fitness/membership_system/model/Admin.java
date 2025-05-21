68e6782d9a94b555044ce6133831c12999d8508b
Tharmithan sachithananthan <96584890+Tharmithan@users.noreply.github.com>
2025-05-21 14:41:26 +0530

    login Admin.java

--- membership-system/src/main/java/com/fitness/membership_system/model/Admin.java
+++ membership-system/src/main/java/com/fitness/membership_system/model/Admin.java
@@ -1,7 +1,8 @@
 package com.fitness.membership_system.model;
-
+//Admin part
 public class Admin extends User {
     private boolean isSuperAdmin;
+
 
     public Admin(String id, String username, String password, String email, String phone, boolean isSuperAdmin) {
         super(id, username, password, email, phone, "Admin");

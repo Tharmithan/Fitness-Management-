9b152f499b885993bb1aaeff2d3043a708d9a7a4
Tharmithan sachithananthan <96584890+Tharmithan@users.noreply.github.com>
2025-05-21 14:54:25 +0530

    Small changes Admin.java

--- membership-system/src/main/java/com/fitness/membership_system/model/Admin.java
+++ membership-system/src/main/java/com/fitness/membership_system/model/Admin.java
@@ -12,8 +12,5 @@ public class Admin extends User {
     public boolean isSuperAdmin() { return isSuperAdmin; }
     public void setSuperAdmin(boolean superAdmin) { isSuperAdmin = superAdmin; }
 
-    @Override
-    public String getRole() {
-        return isSuperAdmin ? "SuperAdmin" : "Admin";
-    }
+   
 }
